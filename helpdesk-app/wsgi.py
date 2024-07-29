906182bf89bdab0d4cf8e009f4424c0981c63070
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-07-29 15:13:14 -0400

    update wsgi

--- helpdesk-app/wsgi.py
+++ helpdesk-app/wsgi.py
@@ -1,9 +1,9 @@
 import sys
 import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'helpdesk-app'))
+
 from app import create_app
-
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 app = create_app()
 
 if __name__ == "__main__":
