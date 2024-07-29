4ea4765dba8d36d6cdcdd189701b1329ce888cfa
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-07-29 14:48:20 -0400

    update

--- helpdesk-app/app/config.py
+++ helpdesk-app/app/config.py
@@ -4,5 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///tickets.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
