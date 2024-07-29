ea3515f14fb3ac4484070d316561ec44c986459d
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-07-29 13:10:12 -0400

    update

--- helpdesk-app/app/__init__.py
+++ helpdesk-app/app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     from .api.comment_routes import comments_bp
     app.register_blueprint(tickets_bp, url_prefix='/tickets')
     app.register_blueprint(comments_bp, url_prefix='/comments')
-
+    
     @app.route('/')
     def index():
         return app.send_static_file('index.html')
